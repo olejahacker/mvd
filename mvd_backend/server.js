8d2aa2488ae92d432ad85523210792ccdecbc350
gettich <get.true01@gmail.com>
2021-04-30 18:53:15 +0300

    Preparing

--- mvd_backend/server.js
+++ mvd_backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const ejs = require('ejs');
 global.__basedir = __dirname;
 
 const initRoutes = require("./src/routes"); 
-
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
--- mvd_backend/src/routes/index.js
+++ mvd_backend/src/routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+
 const router = express.Router();
-const controller = require("../controller/file.controller");
+const controller = require("../controller/controller");
+
 
 let routes = (app) => {
   router.post("/checkImage", controller.upload);
   router.post("/checkText", controller.checkText);
+  router.get("/", controller.index)
+  router.get("/image", controller.image);
   app.use(router);
 };
 
